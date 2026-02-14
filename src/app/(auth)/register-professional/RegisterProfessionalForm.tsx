603a76a4227227f95009a4cd6a035d0c1b29e74a
Ali Rasouli <75220661+alirasouliwebdeveloper@users.noreply.github.com>
2026-02-14 11:08:44 +0400

    fix bug

--- src/app/(auth)/register-professional/RegisterProfessionalForm.tsx
+++ src/app/(auth)/register-professional/RegisterProfessionalForm.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import Link from "next/link";
-import { useState } from "react";
-import Image from "next/image";
+// import Link from "next/link";
+// import { useState } from "react";
+// import Image from "next/image";
 
 export default function RegisterProfessionalForm () {
-
+ return (
+	 <p>RegisterProfessionalForm</p>
+ );
 }
